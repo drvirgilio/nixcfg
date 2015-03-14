6fd112f30c2bf72f10603fe7956db410b77214a0
drvirgilio <drvirgilio@gmail.com>
2015-03-13 19:13:49 -0500

    fix tabs

--- laptop/services.nix
+++ laptop/services.nix
@@ -2,60 +2,65 @@
 
 {
 
-	config.programs.ssh.startAgent = false;
+  config.programs.ssh.startAgent = false;
 
-	######## SERVICES ##########
-	config.services = {
-		acpid = {
-			enable = true;
-#			powerEventCommands = ''
-#				# When the power button is pressed, go to sleep
-#				echo -n mem > /sys/power/state
-#			'';
-		};
-		printing.enable = true;
-		mpd.enable = false;
-		nixosManual.showManual = true;
-		transmission.enable = false;
-		openssh = {
-			enable = false;
-			permitRootLogin = "no";
-		};
-		xserver = {
-			videoDrivers = ["intel"];
-			enable = true;
-			displayManager.slim.enable = true;
-			displayManager.sessionCommands = ''
-				if ! $(pidof -x "gpg-agent" >/dev/null); then
-					eval $(gpg-agent --daemon --enable-ssh-support \
-						--write-env-file "~/.gpg-agent-info")
-				fi
-			'';
-			desktopManager.xterm.enable = false;
-			desktopManager.kde4.enable = true;
-			windowManager.herbstluftwm.enable = true;
-			defaultDepth = 24;
-			autorun = false;
-			exportConfiguration = true;
-			xkbOptions = "terminate:ctrl_alt_bksp, numpad:mac";
-			synaptics = {
-				enable = true;
-				#dev = "/dev/input/by-path/platform-i8042-serio-1-event-mouse";
-				dev = "/dev/input/event6";
-				accelFactor = "0.05";
-				maxSpeed = "10";
-				twoFingerScroll = true;
-				additionalOptions =
-				''
-					Option "HorizTwoFingerScroll" "1"
-					Option "VertTwoFingerScroll"  "1"
-					Option "VertScrollDelta"      "-80"
-					Option "HorizScrollDelta"     "-70"
-					#Option "CircularScrolling"    "on"
-					#Option "CircScrollTrigger"    "0"
-				'';
-			};
-		};
-	};
+  ######## SERVICES ##########
+  config.services = {
+    acpid.enable = true;
+    mpd.enable = false;
+    nixosManual.showManual = true;
+    openssh = {
+      enable = false;
+      permitRootLogin = "no";
+    };
+    printing = {
+      enable = true;
+      #drivers = [ pkgs.gutenprint ];
+    };
+    transmission.enable = false;
+    udisks2.enable = true;
+    xserver = {
+      videoDrivers = ["intel"];
+      enable = true;
+      #startGnuPGAgent = true;
+      displayManager.slim.enable = true;
+      desktopManager.xterm.enable = false;
+      desktopManager.kde4.enable = true;
+      desktopManager.default = "none";
+      windowManager.default = "xmonad";
+      #windowManager.herbstluftwm.enable = true;
+      windowManager.xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+        extraPackages = self: [
+          #self.xmonad
+          self.xmonad-contrib
+          self.xmonad-extras
+          self.xmobar
+        ];
+      };
+      defaultDepth = 24;
+      autorun = true;
+      exportConfiguration = true;
+      xkbOptions = "terminate:ctrl_alt_bksp, numpad:mac";
+      synaptics = {
+        enable = true;
+        #dev = "/dev/input/by-path/platform-i8042-serio-1-event-mouse";
+        dev = "/dev/input/event4";
+        accelFactor = "0.05";
+        maxSpeed = "10";
+        twoFingerScroll = true;
+        additionalOptions =
+        ''
+          Option "HorizTwoFingerScroll" "1"
+          Option "VertTwoFingerScroll"  "1"
+          Option "VertScrollDelta"      "-80"
+          Option "HorizScrollDelta"     "-70"
+          #Option "CircularScrolling"    "on"
+          #Option "CircScrollTrigger"    "0"
+        '';
+      };
+    };
+  };
 }
 
