9cbee186e07ae6704a830ba9e3af1f812d580092
drvirgilio <drvirgilio@gmail.com>
2015-03-21 21:50:47 -0500

    fix install script

--- laptop/install.sh
+++ laptop/install.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-# This script is used to copy the config files and install
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root" 1>&2
-  exit 1
-fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp -r $DIR/* /etc/nixos/
+export NIXOS_CONFIG=$DIR/configuration.nix
+
 nixos-rebuild $*
 
